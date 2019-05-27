e430b261eff2ea2bfb60ccb1f89e6515752287cf
Adam <Adam@LAPTOP-6S52328O>
2019-05-27 15:33:47 -0700

    Adds validation structure and input limitation Objects were created on the initial state of board.tsx to facilitate row, column and block validations. The handleChange function also now includes input limitations to only allow numbers.

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List } from 'immutable'
+import { List, Map } from 'immutable'
 import Cell from './cell'
 
 const initialState = {
@@ -14,6 +14,41 @@ const initialState = {
     List(['', '', '', '', '', '', '', '', '']),
     List(['', '', '', '', '', '', '', '', '']),
   ]),
+  validation: Map({
+    row: List([
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+    ]),
+    column: List([
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+    ]),
+    block: List([
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+      List([]),
+    ]),
+  }),
 }
 
 type State = Readonly<typeof initialState>
@@ -26,7 +61,21 @@ export class InputBoard extends React.Component<object, State> {
     row: number,
     column: number
   ) {
-    const newValue = event.target.value
+    // Limits how inputs are handled. Typing a number in an already occupied
+    // cell replaces it with that number. Typing a non-number deletes what's
+    // in the cell
+    const validCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let newValue: string
+    if (event.target.value.charAt(1)) {
+      newValue = validCharacters.includes(event.target.value.charAt(1))
+        ? event.target.value.charAt(1)
+        : ''
+    } else {
+      newValue = validCharacters.includes(event.target.value.charAt(0))
+        ? event.target.value.charAt(0)
+        : ''
+    }
+
     //TODO: board.get(row) is failing TypeScript compiling with strictNullChecks
     //      activated. Figure out how to set it up correctly so it can be turned
     //      back on.
@@ -39,7 +88,7 @@ export class InputBoard extends React.Component<object, State> {
 
   render() {
     return (
-      <div className="board">
+      <form className="board">
         {this.state.board.map((row, rowNumber) => {
           return (
             <div key={rowNumber} className="row">
@@ -60,7 +109,7 @@ export class InputBoard extends React.Component<object, State> {
             </div>
           )
         })}
-      </div>
+      </form>
     )
   }
 }
